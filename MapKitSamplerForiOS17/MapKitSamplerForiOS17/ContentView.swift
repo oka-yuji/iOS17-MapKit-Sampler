650942f07809af4f060456ae8d0d5b5e77b9089d
岡優志 <yu.j.0513@gmail.com>
2024-03-11 22:44:41 +0900

    Initial Commit

---
+++ MapKitSamplerForiOS17/MapKitSamplerForiOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitSamplerForiOS17
+//
+//  Created by yuji on 2024/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitSamplerForiOS17/MapKitSamplerForiOS17/MapKitSamplerForiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSamplerForiOS17App.swift
+//  MapKitSamplerForiOS17
+//
+//  Created by yuji on 2024/03/11.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSamplerForiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
