87918e529755ebcc1985c1b0a3eedddb72fe99c5
岡優志 <yu.j.0513@gmail.com>
2024-03-13 00:47:11 +0900

    Add navigation MapStyle in ContentView

--- MapKitSamplerForiOS17/MapKitSamplerForiOS17/ContentView.swift
+++ MapKitSamplerForiOS17/MapKitSamplerForiOS17/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                NavigationLink("MapStyle Sample") {
+                    MapStyle()
+                        .navigationBarTitleDisplayMode(.inline)
+                }
+            }
+            .navigationTitle("MapKitSampler")
         }
-        .padding()
     }
 }
 
