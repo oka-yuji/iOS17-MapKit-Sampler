e397726426212216de0b20594345075d26a55eed
岡優志 <yu.j.0513@gmail.com>
2024-03-28 02:13:52 +0900

    fix MapCameraSample

--- MapKitSamplerForiOS17/MapKitSamplerForiOS17/MapCameraSample.swift
+++ MapKitSamplerForiOS17/MapKitSamplerForiOS17/MapCameraSample.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import MapKit
 
 struct MapCameraSample: View {
-    @State private var cameraPosition: MapCameraPosition = .camera(MapCamera(centerCoordinate: .tokyoStation, distance: 100000000))
     @State private var userCameraPosition: MapCameraPosition = .userLocation(followsHeading: true,
                                                                              fallback: .camera(MapCamera(centerCoordinate: .tokyoStation,
                                                                                                          distance: 5000,
